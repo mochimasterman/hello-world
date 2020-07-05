1cf1358428ec58c35b210665d351e720a2df7e7f
Will Hickey <WillHickey@iMac.local>
2020-07-05 15:26:10 -0700

    more de bugs

--- mathtime.py
+++ mathtime.py
@@ -1,30 +1,30 @@
 print("hello!")
 print("lets do math!")
-score = 0
+score = "0"
 answer1 = input("What is 1 + 1?")
 
 if answer1 == "2":
     print("Correct!")
     score = score+1
 else:
-    print ("Incorrect!")
-print("Your score is",answer1)
+    print("Incorrect!")
+print("Your score is", answer1)
 #start level 2
 print("To level 2!")
 answer2 = input("what is 7 plus 2?")
-if answer2 == 9:
-    print ("correct!")
+if answer2 == "9":
+    print("correct!")
     score += 1
 else:
-    print ("Incorrect!")
+    print("Incorrect!")
 
 print("its getting harder!")
-if answer2 == 9 and answer1 == 2:
+if answer2 == "9" and answer1 == "2":
     print("If you get this question correct you will get double points!")
 
 answer3 = input("What is 10 - 6?")
-if answer3 == 4:
-    if answer2 == 9 and answer1 == 2:
+if answer3 == "4":
+    if answer2 == "9" and answer1 == "2":
         print("yay! you got all three questions correct. You have acheived a streak of 3. Keep on going for more prizes! :)")
         score += 2
     else:
@@ -32,5 +32,5 @@ if answer3 == 4:
         score += 1
 else:
     print("wrong! good try!")
-print ("Your score is ",score)
-print ("This is the end for now! Your score was ",score1,"! I update this a few times too! so don't worry!")
+print("Your score is ", score)
+print("This is the end for now! Your score was " ,score, "! I update this a few times too! so don't worry!")
