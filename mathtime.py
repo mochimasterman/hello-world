13942a6e3c67808a6f6f4f2fffa1436765c46785
Will Hickey <WillHickey@iMac.local>
2020-07-05 15:15:24 -0700

    bye bugs!

--- mathtime.py
+++ mathtime.py
@@ -2,7 +2,8 @@ print("hello!")
 print("lets do math!")
 score = 0
 answer1 = input("What is 1 + 1?")
-if answer1 == 2:
+
+if answer1 == "2":
     print("Correct!")
     score = score+1
 else:
@@ -16,9 +17,11 @@ if answer2 == 9:
     score += 1
 else:
     print ("Incorrect!")
+
 print("its getting harder!")
 if answer2 == 9 and answer1 == 2:
     print("If you get this question correct you will get double points!")
+
 answer3 = input("What is 10 - 6?")
 if answer3 == 4:
     if answer2 == 9 and answer1 == 2:
